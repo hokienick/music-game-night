1ada1df0918bfaefc6b5cde80b5a472bfff6d3ee
Nick Castillo <hokienick@DJHokienick.local>
2024-11-29 14:49:06 -0800

    switching to npm build

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
-export default {
-    root: './', // Define the root as 'public'
-    build: {
-        outDir: '../dist',
-        rollupOptions: {
-            input: { 
-                main: './public/index.html',
-                login: './public/admin/login.html',
-            },
-        },
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  root: './public', // Set public as the root directory
+  build: {
+    outDir: '../dist', // Output built files into the dist folder outside public
+    rollupOptions: {
+      input: {
+        main: './public/index.html', // Explicitly specify entry point
+      },
     },
-};
+  },
+  server: {
+    open: true, // Automatically opens the app in the browser
+  },
+});
